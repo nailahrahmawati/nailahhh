bd9f3c2894d55b535cb2676f8024ebbf7c091088
nailahrahmawati <163146675+nailahrahmawati@users.noreply.github.com>
2024-03-19 20:09:52 +0900

    Add files via upload

---
+++ nai bilngan konversi.py
@@ -0,0 +1,16 @@
+bilangan=int(input("masukan bilangan yang ingin dikonversi :\t"))
+
+konversi1=bin(bilangan)[2:]
+
+konversi2=hex(bilangan)
+
+konversi3=oct(bilangan)
+
+print("Bilangan Biner \t\t: ", konversi1.zfill(8))
+
+print("Bilangan Hexadecimal \t: ", konversi2)
+
+print("Bilangan Octal \t\t: ", konversi3)
+
+print("== Nailah Rahmawati Yunus==")
+print("== 07352311121==")
